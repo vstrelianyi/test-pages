492bfc64b473e6d3ff7e7f8c57e3c451cb2e711a
vahan akopjanian <vahan.bclub@gmail.com>
2020-01-28 16:35:37 +0300

    added turning logo

--- src/components/SliderPage/sliderPagaData.js
+++ src/components/SliderPage/sliderPagaData.js
@@ -8,14 +8,14 @@ const sliderPageData = [
     {
         id: 2,
         image: '/images/image_5.jpg',
-        title: 'I am a blogger and food lover',
-        text: 'And a small river named Duden flows by their place and supplies it with the necessary regeliacia'
+        title: 'The cake that"s make you smile',
+        text: 'Next time I saw that cake, my smile was too big and everybody noticed it'
     },
     {
         id: 3,
         image: '/images/category-1.jpg',
         title: 'Delicious Foods & How to Eat Them',
-        text: 'The small river named Duden flows by their place and supplies it with the necessary regeliacia'
+        text: 'It was the great sunny day and me and my friends were going to play some ball and eat some burgers'
     },
    
 ]
