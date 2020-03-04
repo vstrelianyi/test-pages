334c7fc193d63b008cac0c12ade10eeaa3f532ac
vahan akopjanian <vahan.bclub@gmail.com>
2020-03-04 17:19:54 +0300

    deploy slider images

--- src/components/SliderPage/sliderPagaData.js
+++ src/components/SliderPage/sliderPagaData.js
@@ -1,19 +1,19 @@
 const sliderPageData = [
     {
         id: 1,
-        image: 'images/bg_2.jpg',
+        image: '//images/bg_2.jpg',
         title: 'I am a lover and food lover',
         text: 'A small river named Duden flows by their place and supplies it with the necessary regeliacia'
     },
     {
         id: 2,
-        image: 'images/image_5.jpg',
+        image: '//images/image_5.jpg',
         title: 'The cake that"s make you smile',
         text: 'Next time I saw that cake, my smile was too big and everybody noticed it'
     },
     {
         id: 3,
-        image: 'images/category-1.jpg',
+        image: '//images/category-1.jpg',
         title: 'Delicious Foods & How to Eat Them',
         text: 'It was the great sunny day and me and my friends were going to play some ball and eat some burgers'
     },
