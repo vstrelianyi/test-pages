6316ca13ee1bd61171be5549069799b89dcf7344
vahan akopjanian <vahan.bclub@gmail.com>
2020-03-04 21:30:38 +0300

    images to deploy

--- src/components/SliderPage/sliderPagaData.js
+++ src/components/SliderPage/sliderPagaData.js
@@ -1,21 +1,21 @@
 const sliderPageData = [
     {
         id: 1,
-        image: '/food_app_deploy/images/bg_2.jpg',
-        title: 'I am a lover and food lover',
+        image: '../images/bg_2.jpg',
+        title: 'I am a ',
         text: 'A small '
     },
     {
         id: 2,
-        image: '/food_app_deploy/images/image_5.jpg',
+        image: './images/image_5.jpg',
         title: 'The cake that"s make you smile',
         text: 'Next time '
     },
     {
         id: 3,
-        image: '/food_app_deploy/images/category-1.jpg',
+        image: '/images/category-1.jpg',
         title: 'Delicious Foods & How to Eat Them',
-        text: 'It was the great '
+        text: 'It was the great sunny day and me and my friends were going to play some ball and eat some burgers'
     },
    
 ]
