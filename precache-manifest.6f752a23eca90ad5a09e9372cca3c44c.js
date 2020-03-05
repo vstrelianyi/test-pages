2ca83e824328815846b13f2973e5681ab63be47c
Viktor Strelianyi <chmpain@gmail.com>
2020-03-05 22:10:23 +0200

    Updates

---
+++ precache-manifest.6f752a23eca90ad5a09e9372cca3c44c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23389a719a0c32b62bb33a70188e1ee0",
+    "url": "/test-pages/index.html"
+  },
+  {
+    "revision": "3b55403b99e171e4d2dc",
+    "url": "/test-pages/static/css/2.4ebc5633.chunk.css"
+  },
+  {
+    "revision": "502fd98ac6cd42eadaf5",
+    "url": "/test-pages/static/css/main.8ada91ae.chunk.css"
+  },
+  {
+    "revision": "3b55403b99e171e4d2dc",
+    "url": "/test-pages/static/js/2.b560f7a6.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/test-pages/static/js/2.b560f7a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "502fd98ac6cd42eadaf5",
+    "url": "/test-pages/static/js/main.801ac2e6.chunk.js"
+  },
+  {
+    "revision": "a3efaf1d942adabcf1b0",
+    "url": "/test-pages/static/js/runtime-main.ba38e7df.js"
+  },
+  {
+    "revision": "f0bb9f93d20c5dc8f0bf5deeb48f710b",
+    "url": "/test-pages/static/media/1.f0bb9f93.jpg"
+  },
+  {
+    "revision": "b7caa2556521d673aa01512dd412d95d",
+    "url": "/test-pages/static/media/arrowToRight.b7caa255.svg"
+  },
+  {
+    "revision": "b7dbcaad461d6cc89e7460fab57c35b1",
+    "url": "/test-pages/static/media/author.b7dbcaad.jpg"
+  },
+  {
+    "revision": "1b8fac283cd37f95de162f97ede842c4",
+    "url": "/test-pages/static/media/bg_2.1b8fac28.jpg"
+  },
+  {
+    "revision": "6ba54e47453d45a2cf307f6c3568add7",
+    "url": "/test-pages/static/media/bg_3.6ba54e47.jpg"
+  },
+  {
+    "revision": "43268711718843cd621f877d4e06927a",
+    "url": "/test-pages/static/media/bg_4.43268711.jpg"
+  },
+  {
+    "revision": "16fd90be6705f6fa81d309fefe1e9e77",
+    "url": "/test-pages/static/media/bg_5.16fd90be.jpg"
+  },
+  {
+    "revision": "d3078c7f91c2646f85ddf2ca9ecd38c7",
+    "url": "/test-pages/static/media/email-envelope-button.d3078c7f.svg"
+  },
+  {
+    "revision": "a8f4bf4714dd41751fb294c6a418618f",
+    "url": "/test-pages/static/media/facebook.a8f4bf47.svg"
+  },
+  {
+    "revision": "edbdd8cc3b9d4ccd1eaa74980a1f9f9d",
+    "url": "/test-pages/static/media/instagram.edbdd8cc.svg"
+  },
+  {
+    "revision": "1d59e3c78fae275fc455afc5587ff6ea",
+    "url": "/test-pages/static/media/magnifying-glass.1d59e3c7.svg"
+  },
+  {
+    "revision": "328e0f25800f9d33b604a3c9644b5f6e",
+    "url": "/test-pages/static/media/pin.328e0f25.svg"
+  },
+  {
+    "revision": "2be1e204ebd146af9c0f79f2afc34ced",
+    "url": "/test-pages/static/media/play-button-arrowhead.2be1e204.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/test-pages/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/test-pages/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/test-pages/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/test-pages/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b0324aad8658499adc70a94ab6d9de10",
+    "url": "/test-pages/static/media/telegram.b0324aad.svg"
+  },
+  {
+    "revision": "bbe88ba75895bdd3c617bb4b90ad3acf",
+    "url": "/test-pages/static/media/telephone-handle-silhouette.bbe88ba7.svg"
+  },
+  {
+    "revision": "5790426f54c17e801c37e7890d5d7cc7",
+    "url": "/test-pages/static/media/twitter.5790426f.svg"
+  }
+]);
